62d93822e1f7f09a4fa251a236bad5848ee7e33c
Sacha Best <sachabest@outlook.com>
2015-03-24 10:21:37 -0400

    fixing some string bugs, still not working

--- github-groupadder.py
+++ github-groupadder.py
@@ -23,32 +23,33 @@ teams_url = '/teams'
 memberships_url = '/memberships/'
 
 def map_names_ids(token, org_name):
-	url = template_url + orgs_url + org_name + teams_url
+	# to kill pagination assume 500 repos max - change to whatever
+	url = template_url + orgs_url + org_name + teams_url + '?per_page=500'
 	headers = {'Authorization': ' token ' + token, 'Content-type': 'application/json', 'Accept': 'application/vnd.github.v3+json'}
 	r = requests.get(url, headers=headers)
-	json_result = json.dumps(r.text)
+	json_result = json.loads(r.text)
 	team_map = dict()
-	for team_data in json_result: 
-		if team_data['name'].contains('Group'):
-			team_map[team_data['name']] = team_data['id']
+	for team_data in json_result: team_map[team_data['name']] = team_data['id']
 	return team_map
 
 def add_member(token, org_name, team_map, username, team_number):
 	headers = {'Authorization': ' token ' + token, 'Content-type': 'application/json', 'Accept': 'application/vnd.github.v3+json'}
-	team_id = team_map['/Group' + team_number]
-	add_member_url = template_url + orgs_url + org_name + teams_url + team_id + memberships_url + username
+	team_id = team_map[team_number]
+	add_member_url = template_url + orgs_url + org_name + teams_url + str(team_id) + memberships_url + username
 	r = requests.put(add_member_url, headers=headers)
-	print(r.read)
+	print(r.text)
 
 def parse_csv(token, org_name, csv_file):
 	reader = csv.reader(csv_file)
 	team_map = map_names_ids(token, org_name)
-	for row in reader: add_member(token, org_name, team_map, row.split(',')[0], 'Group' + row.split(',')[1])
+	for row in reader: 
+		print(row)
+		add_member(token, org_name, team_map, row[0], 'Group' + row[1])
 
 # main method
 def main():
 	if len(sys.argv) < 4:
-		print 'Usage: github-groupmaker.py org_name csv_file oauth_key'
+		print 'Usage: github-groupadder.py org_name csv_file oauth_key'
 		exit()
 	org_name = sys.argv[1]
 	oauth_key = sys.argv[3]
